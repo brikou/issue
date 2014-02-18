26446723d0ac20e6a750f9bda324e9ea27a9a0be
Brikou CARRE <brikou@gmail.com>
2014-02-18 11:26:36 +0100

    Create Issue1854.php

---
+++ src/My/Issue1854.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace My;
+
+class Issue1854
+{
+    /**
+     * @thisIsAnAnnotationWith1stLetterLowercased
+     */
+    public function my1stMethod()
+    {
+    }
+
+    /**
+     * @ThisIsAnAnnotationWith1stLetterUppercased
+     */
+    public function my2ndMethod()
+    {
+    }
+}
