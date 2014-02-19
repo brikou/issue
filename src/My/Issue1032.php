b9f4fc76532560bd61e6bf41e0f66e17ff167082
Brikou CARRE <brikou@gmail.com>
2014-02-19 09:51:06 +0100

    Create Issue1032.php

---
+++ src/My/Issue1032.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace My;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="test")
+ */
+class TestEntity
+{
+    use Issue1058;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+}
