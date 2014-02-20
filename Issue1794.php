fcb343a458bc0d35599bf8374222425a18a2f126
Brikou CARRE <brikou@gmail.com>
2014-02-20 14:30:20 +0100

    Update Issue1794.php

--- Issue1794.php
+++ Issue1794.php
@@ -9,8 +9,14 @@ class Issue1794 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $i = 0;
-        $this->addSql('UPDATE account_user SET preference_id = CURRVAL(\'account_user_preference_id_seq\') WHERE uuid = (SELECT uuid FROM account_user WHERE preference_id IS NULL LIMIT 1 OFFSET '.$i.')');
+        $sql = "SELECT count(*) FROM account_user WHERE preference_id IS NULL";
+        $stmt = $this->connection->query($sql);
+        $total = $stmt->fetchColumn();
+
+        for ($i = 0; $i < $total; $i++) {
+            $this->addSql('INSERT INTO account_user_preference (id, notify_grade_changed) VALUES (NEXTVAL(\'account_user_preference_id_seq\'), true)');
+            $this->addSql('UPDATE account_user SET preference_id = CURRVAL(\'account_user_preference_id_seq\') WHERE uuid = (SELECT uuid FROM account_user WHERE preference_id IS NULL LIMIT 1 OFFSET '.$i.')');
+        }
     }
 
     public function down(Schema $schema)
